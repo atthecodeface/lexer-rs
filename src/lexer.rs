ea581267b16d4e4c6be528297cc664e7f53a9f7f
Gavin J Stark <atthecodeface@gmail.com>
2022-04-24 17:32:09 +0100

    Moved lexer to submodule

---
+++ src/lexer.rs
@@ -0,0 +1,13 @@
+//a Documentation
+//a Imports
+mod pos;
+mod simple;
+mod text_stream;
+
+pub use pos::{LineCol, Pos, Span, TextPos};
+pub use simple::SimpleToken;
+pub use text_stream::{
+    TextStream, TextStreamSpan, TextStreamSpanIterator, TokenParseError, TokenParseResult,
+    TokenParser, TokenType, TokenTypeError,
+};
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 //a Documentation
 //a Imports
-mod pos;
-mod simple;
-mod text_stream;
+mod lexer;
 
-pub use pos::{LineCol, Pos, Span, TextPos};
-pub use simple::SimpleToken;
-pub use text_stream::{
+pub use lexer::{LineCol, Pos, Span, TextPos};
+pub use lexer::SimpleToken;
+pub use lexer::{
     TextStream, TextStreamSpan, TextStreamSpanIterator, TokenParseError, TokenParseResult,
     TokenParser, TokenType, TokenTypeError,
 };
