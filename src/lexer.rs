50f72855864484bbf090d29d4a36652bb40d1812
Gavin J Stark <atthecodeface@gmail.com>
2022-09-18 14:20:57 +0100

    Moved span out

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,12 +3,14 @@
 mod pos;
 mod line_column;
 mod simple;
+mod span;
 mod traits;
 mod text_stream;
 
 pub use line_column::LineColumn;
 pub use traits::{PosnInStream, TokenType, TokenTypeError};
-pub use pos::{Pos, Span};
+pub use pos::Pos;
+pub use span::Span;
 pub use simple::{SimpleKeyword, SimpleToken};
 pub use text_stream::{
     TextStreamSpan, TextStreamSpanIterator, TokenParseError, TokenParseResult, TokenParser,
--- src/lexer/pos.rs
+++ src/lexer/pos.rs
@@ -47,34 +47,3 @@ where
     }
 }
 
-//a Span
-//tp Span
-/// A span within a str
-#[derive(Debug, Copy, Clone)]
-pub struct Span<P>
-where
-    P: PosnInStream,
-{
-    start: Pos<P>,
-    end: Pos<P>,
-}
-
-//ip Span
-impl<P> Span<P>
-where
-    P: PosnInStream,
-{
-    //fp new
-    /// Create a new [Span]
-    pub fn new(start: Pos<P>, end: Pos<P>) -> Self {
-        Self { start, end }
-    }
-
-    //mp byte_range
-    /// Get the range between the two positions of this [Span]
-    pub fn byte_range(&self) -> Range<usize> {
-        let start = self.start.byte_ofs;
-        let end = self.end.byte_ofs;
-        Range { start, end }
-    }
-}
