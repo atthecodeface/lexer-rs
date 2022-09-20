932e5003f25fe1881a4bcefd008a056802952bff
Gavin J Stark <atthecodeface@gmail.com>
2022-09-20 10:13:43 +0100

    Moved position and span things to submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,22 @@
 //a Documentation
 /// remove TokenType and use Lexer::Token instead
 //a Imports
-mod line_column;
+mod posn_and_span;
 mod parser_iter;
-// mod simple;
-mod stream_char_pos;
-mod stream_char_span;
 mod text_lexer;
-// mod text_stream;
 mod token_parse_error;
 mod traits;
-
-pub use line_column::LineColumn;
-pub use parser_iter::ParserIterator;
+// mod simple;
+// mod text_stream;
 // pub use simple::{SimpleKeyword, SimpleToken};
-pub use stream_char_pos::StreamCharPos;
-pub use stream_char_span::StreamCharSpan;
+
+pub use posn_and_span::LineColumn;
+pub use posn_and_span::StreamCharPos;
+pub use posn_and_span::StreamCharSpan;
+pub use posn_and_span::{PosnInCharStream, PosnInStream};
+
+pub use parser_iter::ParserIterator;
 pub use text_lexer::TSSLexer;
 pub use token_parse_error::LexerParseError;
 pub use traits::{Lexer, LexerError, LexerOfChar, LexerParseFn, LexerParseResult};
-pub use traits::{PosnInCharStream, PosnInStream};
 pub use traits::BoxDynLexerPasrseFn;
